7df06fbc03eb6bdedc321cd3aa0062ab4c581688
✝-♫ √ɑŀҡɨʀɨ Ҝ-ƁѺøɱ ♪-✝ <-  - ->
2017-01-21 15:33:54 +0100

    MWAHAHAHAHAHHAH

--- Assets/Scripts/Commands/ClickHandler.cs
+++ Assets/Scripts/Commands/ClickHandler.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class ClickHandler : MonoBehaviour {
   public Transform spherePrefab;
-  public LayerMask myLayerMask;
-
-  void Update () {
+    int layerMask;
+    void Start()
+    {
+        layerMask = LayerMask.GetMask("Ground");
+    }
+  void FixedUpdate () {
     if (Input.GetMouseButtonDown(0)) {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit,myLayerMask)) {
+        if (Physics.Raycast(ray, out hit,100.0f, layerMask)) {
                 Debug.Log(hit.collider.tag);
             if (hit.collider.tag == "Ground") {
                 Transform sphere = Instantiate(spherePrefab) as Transform;
