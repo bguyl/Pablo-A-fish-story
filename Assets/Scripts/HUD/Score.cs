2d2f62d5594cf285bdd1a325d6f1fb7da2c19925
Kevin Ilphrin Pellet <ilphrin@autistici.org>
2017-01-22 12:21:02 +0100

    Add some scoring

---
+++ Assets/Scripts/HUD/Score.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+  public Transform GameManager;
+  public Transform MainCamera;
+
+  private int totalScore;
+  private int currentScore;
+  private Text text;
+
+  // Use this for initialization
+  void Start () {
+    text = GetComponentsInChildren<Text>()[0];
+  }
+
+  // Update is called once per frame
+  void Update () {
+    
+  }
+}
